4b09193efccd0835b8204f03e0805bea69856cf2
ocnelavoc <ocnelavoc@gmail.com>
2023-01-23 10:01:26 +0200

    add controller

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 console.log(`hello`);
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 const morgan = require('morgan');
-const app = express()
-app.use(morgan('combine'))
+const app = express();
+app.use(morgan('combine'));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 app.post('/register', (req, res) => {
   res.send({
-    message:`Hello ${req.body.email}! Пользователь зарегистрирован!`
-  })
-})
-app.listen(process.env.PORT || 8081)
+    message: `Hello ${req.body.email}! Пользователь зарегистрирован!`
+  });
+});
+app.get('/register', (req, res) => {
+  res.send({
+    message: `запрос Get register`
+  });
+});
+app.get('/status', (req, res) => {
+  res.send({
+    message: `Hello ! Пользователь зарегистрирован!`
+  });
+});
+app.listen(process.env.PORT || 8081);
